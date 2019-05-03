2a8af09e4f02bba9bf27f7540224a397db3150cb
vunht <vunht@VID-VUNHT-L.Vingroup.local>
2019-05-03 14:17:23 +0700

    update

--- src/com/regexbasic/SplitRegexNumber.java
+++ src/com/regexbasic/SplitRegexNumber.java
@@ -3,7 +3,7 @@ package com.regexbasic;
 public class SplitRegexNumber {
 
 	public static void main(String[] args) {
-		String TEXT = "1000,000,000";
+		String TEXT = "1000,---0000,0000.100:'''']====";
 		System.out.println("TEXT=" + TEXT);
 
 		String newText = TEXT.replaceAll("\\D", "");
