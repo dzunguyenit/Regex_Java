b52f81f8333ee0edb303ce62bf57001fb2ba515d
vunht <vunht@VID-VUNHT-L.Vingroup.local>
2019-05-03 12:16:48 +0700

    update

--- src/com/regexbasic/SplitRegexNumber.java
+++ src/com/regexbasic/SplitRegexNumber.java
@@ -5,19 +5,10 @@ public class SplitRegexNumber {
 	public static void main(String[] args) {
 		String TEXT = "1000,000,000";
 		System.out.println("TEXT=" + TEXT);
-		// Khoảng trắng xuất hiện 1 hoặc nhiều lần.
-		// Các ký tự khoảng trắng: \t\n\x0b\r\f
-		// Kết hợp quy tắc: \s và +
-		String regex = "\\,";
-		String[] splitString = TEXT.split(regex);
-		System.out.println(splitString.length);
 
-		for (String string : splitString) {
-			System.out.println(string);
-		}
+		String newText = TEXT.replaceAll("(\\D+?)", "");
 
-		// Thay thế tất cả các khoảng trắng với ký tự tab.
-		String newText = TEXT.replaceAll("\\,", "");
+//		String newText = TEXT.replaceAll("\\,", "");
 		System.out.println("New text=" + newText);
 	}
 }
