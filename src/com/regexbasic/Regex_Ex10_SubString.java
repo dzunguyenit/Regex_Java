fb1d7b1e495d9212b077a44a68998d637ea4d7ae
vunht <vunht@VID-VUNHT-L.Vingroup.local>
2019-05-04 19:36:26 +0700

    update

--- src/com/regexbasic/Regex_Ex10_SubString.java
+++ src/com/regexbasic/Regex_Ex10_SubString.java
@@ -5,9 +5,18 @@ import org.apache.commons.lang3.StringUtils;
 public class Regex_Ex10_SubString {
 
 	public static void main(String[] args) {
-		String[] strArray = { "string1", "string2", "string3" };
-		System.out.println("Joining array of strings");
-		System.out.println(StringUtils.join(strArray));
-		System.out.println(StringUtils.join(strArray, " "));
+
+		String text = "I love      cats          and dogs";
+
+		String[] newText = text.split("\\s+");
+	
+		for (String te : newText) {
+			System.out.println(te);
+		}
+
+//		String[] strArray = { "string1", "string2", "string3" };
+//		System.out.println("Joining array of strings");
+		System.out.println(StringUtils.join(newText));
+		System.out.println(StringUtils.join(newText, " "));
 	}
 }
