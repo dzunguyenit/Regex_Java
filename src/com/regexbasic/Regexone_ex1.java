8dd16b3a4d9e224efdd916f3251aa487c3b3d1ef
VuNguyen <dzunguyenit@gmail.com>
2019-05-05 15:57:56 +0700

    up

--- src/com/regexbasic/Regexone_ex1.java
+++ src/com/regexbasic/Regexone_ex1.java
@@ -8,5 +8,13 @@ public class Regexone_ex1 {
 		System.out.println("New TEXT1= " + TEXT1.replaceAll("def*g*", ""));
 		System.out.println("New TEXT2= " + TEXT2.replaceAll("def*g*", ""));
 
+		String TEXT3 = "abc123xyz";
+		String TEXT4 = "define \"123\"";
+		String TEXT5 = "var g = 123;";
+
+		System.out.println("New TEXT3= " + TEXT3.replaceAll("\\D", ""));
+		System.out.println("New TEXT4= " + TEXT4.replaceAll("\\D", ""));
+		System.out.println("New TEXT5= " + TEXT5.replaceAll("\\D", ""));
+
 	}
 }
