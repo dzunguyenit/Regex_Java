9e7d2f7be0d532c63548d435d8afd6f1907c4209
VuNguyen <dzunguyenit@gmail.com>
2019-05-06 23:16:54 +0700

    up

--- src/com/regexbasic/Regex_Ex11_SubStringNumber.java
+++ src/com/regexbasic/Regex_Ex11_SubStringNumber.java
@@ -1,29 +1,17 @@
 package com.regexbasic;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.math.NumberUtils;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Regex_Ex11_SubStringNumber {
 
 	public static void main(String[] args) {
 
-		String text = "123,000.000:999";
-
-		String newText = text.replace("\\D", "");
-
-			System.out.println(newText);
-		String totalString = StringUtils.join(newText);
-		int newNumber = Integer.parseInt(totalString);
-
-//		System.out.println(StringUtils.join(newText, ""));
-//		String totalString = StringUtils.join(newText);
-//		NumberUtils.createInteger(str)
-
-//		int t = NumberUtils.toInt(totalString);
-		System.out.println("tong " + newNumber);
-
-		int tong = (NumberUtils.toInt(totalString)) + 1;
-
-		System.out.println("Tong la: " + tong);
+		String mydata = "some string with 'the data i want' inside";
+		Pattern pattern = Pattern.compile("'(.*?)'");
+		Matcher matcher = pattern.matcher(mydata);
+		if (matcher.find()) {
+			System.out.println(matcher.group(1));
+		}
 	}
 }
