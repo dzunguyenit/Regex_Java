401812a154e05a81e92804a88ade8fe2087df46c
VuNguyen <dzunguyenit@gmail.com>
2019-05-06 22:44:38 +0700

    u

--- src/com/regexbasic/Regexone_ex3.java
+++ src/com/regexbasic/Regexone_ex3.java
@@ -1,24 +1,21 @@
 package com.regexbasic;
 
-public class Regexone_ex2 {
+public class Regexone_ex3 {
 
 	public static void main(String[] args) {
-		String TEXT1 = "tom@hogwarts.com";
+		String TEXT1 = "ABC:*+MN415-555-1234";
 		System.out.println("Before TEXT1= " + TEXT1);
 
-		String TEXT2 = "tom.riddle@hogwarts.com";
+		String TEXT2 = "KL(416)555-3456:TH-";
 		System.out.println("Before TEXT2= " + TEXT2);
-
-		String TEXT3 = "hermione+regexone@hogwarts.com";
+		String TEXT3 = "Lpp202 555 PP} 4567";
 		System.out.println("Before TEXT3= " + TEXT3);
 
-		String newText1 = TEXT1.replaceAll("(@hogwarts.com)$", "");
+		String newText1 = TEXT1.replaceAll("\\D*", "");
 		System.out.println("After newText1= " + newText1);
-
-		String newTEXT2 = TEXT2.replaceAll("(@hogwarts.com)$", "");
+		String newTEXT2 = TEXT2.replaceAll("\\D*", "");
 		System.out.println("After newTEXT2= " + newTEXT2);
-
-		String newTEXT3 = TEXT3.replaceAll("(@hogwarts.com)$*", "");
+		String newTEXT3 = TEXT3.replaceAll("\\D*", "");
 		System.out.println("After newTEXT3= " + newTEXT3);
 
 	}
