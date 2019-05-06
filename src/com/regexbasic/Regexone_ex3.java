b2a721ff8716e878ed5d6896a778f2771e48525e
VuNguyen <dzunguyenit@gmail.com>
2019-05-06 23:01:40 +0700

    q

--- src/com/regexbasic/Regexone_ex3.java
+++ src/com/regexbasic/Regexone_ex3.java
@@ -3,19 +3,19 @@ package com.regexbasic;
 public class Regexone_ex3 {
 
 	public static void main(String[] args) {
-		String TEXT1 = "ABC:*+MN415-555-1234";
+		String TEXT1 = "<a>This is a link</a>";
 		System.out.println("Before TEXT1= " + TEXT1);
 
-		String TEXT2 = "KL(416)555-3456:TH-";
+		String TEXT2 = "<a>This is a link</a>";
 		System.out.println("Before TEXT2= " + TEXT2);
-		String TEXT3 = "Lpp202 555 PP} 4567";
+		String TEXT3 = "<a href='https://regexone.com'>Link</a>";
 		System.out.println("Before TEXT3= " + TEXT3);
 
-		String newText1 = TEXT1.replaceAll("\\D*", "");
+		String newText1 = TEXT1.replaceAll("<(\\w+)>\\w*[\\s\\w]*", "");
 		System.out.println("After newText1= " + newText1);
-		String newTEXT2 = TEXT2.replaceAll("\\D*", "");
+		String newTEXT2 = TEXT2.replaceAll(">([\\w\\s]*)<", "");
 		System.out.println("After newTEXT2= " + newTEXT2);
-		String newTEXT3 = TEXT3.replaceAll("\\D*", "");
+		String newTEXT3 = TEXT3.replaceAll("'([\\w://.]*)'", "");
 		System.out.println("After newTEXT3= " + newTEXT3);
 
 	}
