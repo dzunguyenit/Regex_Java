c560d515e168278ddd43b8f9a9f0a3bb3431af0c
VuNguyen <dzunguyenit@gmail.com>
2019-05-07 00:11:14 +0700

    update

--- src/regexSubString/Regex_Exercise_8.java
+++ src/regexSubString/Regex_Exercise_8.java
@@ -3,10 +3,10 @@ package regexSubString;
 public class Regex_Exercise_8 {
 
 	public static void main(String[] args) {
-		String regex = "(\\w+)\\(([\\w\\.]+):(\\d+)\\)";
-		String TEXT1 = "at widget.List.makeView(ListView.java:1727)";
+		String regex = "(\\w+)://([\\w\\-\\.]+)(:(\\d+))?";
+		String TEXT1 = "ftp://file_server.com:21/top_secret/life_changing_plans.pdf";
 		System.out.println("Before TEXT1= " + TEXT1);
-		String TEXT2 = "at widget.List.fillFrom(ListView.java:709)";
+		String TEXT2 = "market://search/angry%20birds";
 		System.out.println("Before TEXT2= " + TEXT2);
 
 		System.out.println("After TEXT1= " + Regex.getSubString(TEXT1, regex));
