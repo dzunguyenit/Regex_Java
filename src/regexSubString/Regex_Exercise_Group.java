08e9839f46c91b7c07c844d522326cc039ba8645
vunht <vunht@VID-VUNHT-L.Vingroup.local>
2019-07-12 16:13:04 +0700

    up

--- src/regexSubString/Regex_Exercise_Group.java
+++ src/regexSubString/Regex_Exercise_Group.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class Regex_Exercise_Group {
 
 	public static void main(String[] args) {
-		String regex = "(\\d\\s?)(\\d+).*(\\d+)";
+		String regex = "AA(\\d+\\s?)(\\d+),.(\\d+)";
 		String TEXT1 = "AA09233 9991,.99";
 
 		Pattern pattern = Pattern.compile(regex);
@@ -14,16 +14,19 @@ public class Regex_Exercise_Group {
 		if (m.find()) {
 			String total = "";
 			// if our pattern matches the string, we can try to extract our groups
-			if (m.find()) {
-				// get the two groups we were looking for
-				String group1 = m.group(1);
-				String group2 = m.group(2);
+			// get the two groups we were looking for
+			System.out.println(m.group(0));
+			String group1 = m.group(1);
+			System.out.println(m.group(1));
+			String group2 = m.group(2);
+			System.out.println(m.group(2));
+			String group3 = m.group(3);
+			System.out.println(m.group(3));
 
-				total = group1 + group2;
-				System.out.println(total);
-
-			}
+			total = group1 + group2 + group3;
+			System.out.println(total);
 
 		}
+
 	}
 }
