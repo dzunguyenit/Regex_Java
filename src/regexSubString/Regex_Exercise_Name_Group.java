bbbdfed1530167e50bb3104aeec3835bdf54d4c9
vunht <vunht@VID-VUNHT-L.Vingroup.local>
2019-07-12 16:39:41 +0700

    up

--- src/regexSubString/Regex_Exercise_Name_Group.java
+++ src/regexSubString/Regex_Exercise_Name_Group.java
@@ -3,26 +3,20 @@ package regexSubString;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Regex_Exercise_Group {
+public class Regex_Exercise_Name_Group {
 
 	public static void main(String[] args) {
-		String regex = "AA(\\d+\\s?)(\\d+),.(\\d+)";
-		String TEXT1 = "AA09233 9991,.99";
+		String regex = "I have (\\d+) dola";
+		String TEXT1 = "I have 100 dola";
 
 		Pattern pattern = Pattern.compile(regex);
 		Matcher m = pattern.matcher(TEXT1);
 
 		if (m.find()) {
-			String total = "";
 			System.out.println(m.group(0));
-			String group1 = m.group(1);
 			System.out.println(m.group(1));
-			String group2 = m.group(2);
-			System.out.println(m.group(2));
-			String group3 = m.group(3);
-			System.out.println(m.group(3));
 
-			total = group1 + group2 + group3;
+			int total = Integer.parseInt(m.group(1)) + 100;
 			System.out.println(total);
 
 		}
