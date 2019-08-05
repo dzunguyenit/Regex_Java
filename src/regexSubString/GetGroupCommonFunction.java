f3a1ab7c098485d207eee63eda16977e9a46c721
vunht <vunht@VID-VUNHT-L.Vingroup.local>
2019-08-05 12:10:54 +0700

    update

---
+++ src/regexSubString/GetGroupCommonFunction.java
@@ -0,0 +1,24 @@
+package regexSubString;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class GetGroupCommonFunction {
+
+	public static void main(String[] args) {
+		String total = getTextRegex("Showing 11 to 20 of (\\d+) entrie", "Showing 11 to 20 of 57 entrie", 1);
+		System.out.println("Total = " + total);
+
+	}
+
+	public static String getTextRegex(String regex, String text, int group) {
+		String textRegex = "";
+		Pattern patternRegex = Pattern.compile(regex);
+		Matcher m = patternRegex.matcher(text);
+
+		if (m.find()) {
+			textRegex = m.group(group);
+		}
+		return textRegex;
+	}
+}
